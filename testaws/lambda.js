3cd550152f3da45a9cf23cb8c0852ce3a0dd6315
Indunil1992 <indunil@adroitlogic.com>
2019-12-18 11:53:06 +0530

    2qa

--- testaws/lambda.js
+++ testaws/lambda.js
@@ -5,7 +5,7 @@ exports.handler = async (event) => {
     ses.sendEmail({
         Destination: {
             ToAddresses: ['indunil@adroitlogic.com'],
-            CcAddresses: [],
+            CcAddresses: ['sachithrarajapakse1992@gmail.com'],
             BccAddresses: []
         },
         Message: {
@@ -18,7 +18,7 @@ exports.handler = async (event) => {
                 Data: 'test1.3.11'
             }
         },
-        Source: 'indunil@adroitlogic.com',
+        Source: 'indunil@adroitlogic.com'
     }, function (err, data) {
         if (err) console.log(err, err.stack); // an error occurred
         else console.log(data);           // successful response
